5d8c28217a7b6f2bdbf0fd0c113d4700b611da14
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-07-01 20:54:18 +0100

    add unit test

--- firebond/test/rates_test.go
+++ firebond/test/rates_test.go
@@ -58,9 +58,25 @@ func GetAllCryptoCurrencyRate(t *testing.T) {
 
 func GetAccountEthereumBalance(t *testing.T) {
 	router := gin.Default()
-	router.GET("/api/v1/firebond//ethereum/balance/:address", handlers.GetAccountEthereumBalance)
+	router.GET("/api/v1/firebond/ethereum/balance/:address", handlers.GetAccountEthereumBalance)
 
 	req, err := http.NewRequest("GET", "http://localhost:9001/api/v1/firebond/pricing/ethereum/balance/0x0d8775f648430679a709e98d2b0cb6250d2887ef", nil)
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+	res := httptest.NewRecorder()
+	router.ServeHTTP(res, req)
+
+	if res.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, res.Code)
+	}
+}
+
+func GetCryptoCurrencyHistory(t *testing.T) {
+	router := gin.Default()
+	router.GET("/api/v1/firebond/rates/history/:cryptocurrency/:fiat", handlers.GetCryptoCurrencyHistory)
+
+	req, err := http.NewRequest("GET", "http://localhost:9001/api/v1/firebond/pricing/rates/history/bitcoin/eur", nil)
 	if err != nil {
 		t.Fatalf("Failed to create request: %v", err)
 	}

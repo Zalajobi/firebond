56b958dd601d7e2c1af8ce6164ce52996b4f4219
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-07-01 20:33:17 +0100

    unit test for get price

--- firebond/test/rates_test.go
+++ firebond/test/rates_test.go
@@ -8,11 +8,27 @@ import (
 	"testing"
 )
 
-func TestGetCryptoCurrencyRate(t *testing.T) {
+func TestGetPrice(t *testing.T) {
 	router := gin.Default()
 	router.GET("/api/v1/firebond/pricing/rates/:cryptocurrency/:fiat", handlers.GetPrice)
 
 	req, err := http.NewRequest("GET", "http://localhost:9001/api/v1/firebond/pricing/rates/bitcoin/usd", nil)
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+	res := httptest.NewRecorder()
+	router.ServeHTTP(res, req)
+
+	if res.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, res.Code)
+	}
+}
+
+func TestGetCryptocurrencyRates(t *testing.T) {
+	router := gin.Default()
+	router.GET("/api/v1/firebond/pricing/rates/:cryptocurrency", handlers.GetCryptocurrencyRates)
+
+	req, err := http.NewRequest("GET", "http://localhost:9001/api/v1/firebond/pricing/rates/bitcoin", nil)
 	if err != nil {
 		t.Fatalf("Failed to create request: %v", err)
 	}
